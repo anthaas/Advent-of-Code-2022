d938c9bf83a2290860ca9c54b4409c9c63f1135f
Antonin Haas <tondahaas@gmail.com>
2022-12-04 21:08:27 +0100

    add solution for day 4

---
+++ day04/Day04.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace day04
+{
+    class Day04
+    {
+        static void Main(string[] args)
+        {
+            var lines = File.ReadLines(@"input.txt");
+            var sectionsRange = lines.Select(x => x.Split(',', '-').Select(long.Parse)).ToArray();
+            var containsIntervalCount = sectionsRange.Select(sections =>
+            {
+                return (sections.ElementAt(0) <= sections.ElementAt(2) && sections.ElementAt(1) >= sections.ElementAt(3))
+                || (sections.ElementAt(2) <= sections.ElementAt(0) && sections.ElementAt(3) >= sections.ElementAt(1)) ? 1 : 0;
+            }).Sum();
+            Console.WriteLine(containsIntervalCount);
+
+            var overlapsIntervalCount = sectionsRange.Select(sections =>
+            {
+                return (sections.ElementAt(2) <= sections.ElementAt(1) && sections.ElementAt(2) >= sections.ElementAt(0))
+                || (sections.ElementAt(0) <= sections.ElementAt(3) && sections.ElementAt(0) >= sections.ElementAt(2)) ? 1 : 0;
+            }).Sum();
+            Console.WriteLine(overlapsIntervalCount);
+        }
+    }
+}
